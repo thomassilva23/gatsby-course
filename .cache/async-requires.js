5680d60bad167c13ae09888fdf649550cfb7d8f7
Thomas Silva <thomas@allto.digital>
2021-01-20 16:55:35 -0300

    Configurando a busca por posts no Algolia

--- .cache/async-requires.js
+++ .cache/async-requires.js
@@ -2,7 +2,6 @@
 const preferDefault = m => (m && m.default) || m
 
 exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
   "component---src-pages-404-js": () => import("./../../../src/pages/404.js" /* webpackChunkName: "component---src-pages-404-js" */),
   "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
   "component---src-pages-using-typescript-tsx": () => import("./../../../src/pages/using-typescript.tsx" /* webpackChunkName: "component---src-pages-using-typescript-tsx" */),
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\thoma\\Documents\\Development\\my-blog\\.cache\\dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\thoma\\Documents\\Development\\my-blog\\src\\pages\\404.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\thoma\\Documents\\Development\\my-blog\\src\\pages\\about.js"))),
   "component---src-pages-using-typescript-tsx": hot(preferDefault(require("C:\\Users\\thoma\\Documents\\Development\\my-blog\\src\\pages\\using-typescript.tsx"))),
